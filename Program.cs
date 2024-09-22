0ea6f1f85682a6508ecd8a95784d02b900fa30d1
stanlocktop <wlad-test@ukr.net>
2024-09-22 13:52:50 +0300

    commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repeat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello, my name is Vladick:)");
+            Console.ResetColor();
+
+            Console.WriteLine("Напишіть Ім'я");
+            string name = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Я люблю " + name);
+            Console.ReadKey();
+        }
+    }
+}
